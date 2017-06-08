b5650b1ae85eed02b62619414c099af7d2f03ac3
Anton Ukhanev <xedin.unknown@gmail.com>
2017-06-08 23:33:24 +0200

    Added abstract locators

---
+++ src/AbstractModuleLocator.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Dhii\Modular\Locator;
+
+/**
+ * Common functionality for module locators.
+ *
+ * @since [*next-version*]
+ */
+abstract class AbstractModuleLocator
+{
+    /**
+     * Parameter-less constructor.
+     * 
+     * Invoke this in the real constructor.
+     * 
+     * @since [*next-version*]
+     */
+    protected function _construct()
+    {
+    }
+
+    /**
+     * Reads a config source into a standard config.
+     * 
+     * @since [*next-version*]
+     * 
+     * @return array The standard config.
+     */
+    abstract protected function _read($config);
+
+    /**
+     * Retrieves configuration sources.
+     *
+     * @since [*next-version*]
+     *
+     * @return array Configuration sources.
+     */
+    abstract protected function _getSources();
+
+    /**
+     * Locates module configurations.
+     *
+     * @since [*next-version*]
+     *
+     * @return mixed[] A list of module configurations.
+     */
+    protected function _locate()
+    {
+        $configs = array();
+        foreach ($this->_getSources() as $_source) {
+            $configs[md5($_source)] = $this->_read($_source);
+        }
+
+        return $configs;
+    }
+
+    /**
+     * Determines a config key based on its source.
+     *
+     * @since [*next-version*]
+     *
+     * @param mixed $configSource The config source.
+     *
+     * @return string The key that identifies the source.
+     */
+    protected function _generateKeyFromSource($configSource)
+    {
+        return md5($configSource);
+    }
+}
