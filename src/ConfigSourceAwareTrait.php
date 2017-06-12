fa812f9da7b6e73ee9e153d541b0e54e0aec40ec
Anton Ukhanev <xedin.unknown@gmail.com>
2017-06-12 18:47:38 +0200

    Added `ConfigSourceAwareTrait` with test

---
+++ src/ConfigSourceAwareTrait.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Dhii\Modular\Locator;
+
+/**
+ * Functionality that allows setting and retrieval of a module configuration source (protected).
+ *
+ * @since [*next-version*]
+ */
+trait ConfigSourceAwareTrait
+{
+    /**
+     * Assigns the module config source.
+     *
+     * @since [*next-version*]
+     *
+     * @param mixed $source The source.
+     *
+     * @return $this
+     */
+    protected function _setConfigSource($source)
+    {
+        $this->configSource = $source;
+
+        return $this;
+    }
+
+    /**
+     * Retrieves the config source.
+     *
+     * @since [*next-version*]
+     *
+     * @return mixed The source.
+     */
+    protected function _getConfigSource()
+    {
+        return $this->configSource;
+    }
+}
